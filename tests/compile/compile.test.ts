6130807c191838e6e360d750a4e19737fd6385d9
PickleGherkin <gknselcuk@gmail.com>
2025-09-29 16:47:36 +0200

    rewritten tests

--- tests/compile/compile.test.ts
+++ tests/compile/compile.test.ts
@@ -1,25 +1,80 @@
-import { vi } from "vitest";
 import { cliSetup } from "../../src/cliSetup";
+import { Command } from "commander";
+
+let output = "";
+let oldLog: {
+  (...data: any[]): void;
+  (message?: any, ...optionalParams: any[]): void;
+}
 
 beforeEach(() => {
-  cliSetup();
-  program.exitOverride();
-})
-
-test("Show default help message when running 'spookyts'", () => {
-  let output = "";
-  const writeSpy = vi.spyOn(process.stdout, "write").mockImplementation((str) => {
-    output += str;
-    return true;
+  globalThis.program = new Command();
+  program.exitOverride((cmdError) => {
+    throw cmdError;
   });
 
-  expect(() => program.parse(["node", "spookyts.js", "--help"], { from: "user" }))
-    .toThrow();
+  cliSetup();
 
-  expect(writeSpy).toHaveBeenCalled();
+  oldLog = console.log;
+  console.log = (str: string) => { output += str + "\n"; oldLog(str); };
+  program.configureOutput({
+    writeOut: (str: string) => (output += str),
+    writeErr: (str: string) => (output += str),
+  });
+})
+
+afterEach(() => {
+  output = "";
+})
+
+test("Show help message when running 'spookyts --help'", async () => {
+  try {
+    await program.parseAsync(["--help"], { from: "user" });
+  } catch (cmdError) {
+    expect(cmdError.exitCode).toBe(0);
+  } finally {
+    console.log = oldLog;
+  }
   expect(output).toContain("--help");
   expect(output).toContain("--version");
-  expect(output).not.toContain("--dry");
-  expect(output).not.toContain("--uncapped");
-  expect(output).not.toContain("--quiet");
+});
+
+test("Show help message when running 'spookyts'", async () => {
+  try {
+    await program.parseAsync([], { from: "user" });
+  } catch (cmdError) {
+    expect(cmdError.exitCode).toBe(1);
+  } finally {
+    console.log = oldLog;
+  }
+  expect(output).toContain("--help");
+  expect(output).toContain("--version");
+});
+
+test("Show help message when running 'spookyts compile --help'", async () => {
+  try {
+    await program.parseAsync(["compile", "--help"], { from: "user" });
+  } catch (cmdError) {
+    expect(cmdError.exitCode).toBe(0);
+  } finally {
+    console.log = oldLog;
+  }
+  expect(output).toContain("destinations");
+  expect(output).toContain("--dry");
+  expect(output).toContain("--quiet");
+  expect(output).toContain("--uncapped");
+});
+
+test("Show help message when running 'spookyts compile'", async () => {
+  try {
+    await program.parseAsync(["compile"], { from: "user" });
+  } catch (cmdError) {
+    expect(cmdError.exitCode).toBe(0); // this is exit code 0 because a custom hook triggers help
+  } finally {
+    console.log = oldLog;
+  }
+  expect(output).toContain("destinations");
+  expect(output).toContain("--dry");
+  expect(output).toContain("--quiet");
+  expect(output).toContain("--uncapped");
 });
